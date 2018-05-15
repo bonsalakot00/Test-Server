f084a5f9b02f7e3fae315c1e201b929408074c81
bonsalakot00 <asungotdarren@yahoo.com>
2018-05-15 21:38:55 +0800

    Update datasampletest1.py

--- datasampletest1.py
+++ datasampletest1.py
@@ -1,17 +1,19 @@
-import csv
-import io
-import urllib.request
+import csv #csv file module parsing file/data
+import io #I/O reader
+import urllib.request #module for opening URLS
 
-url = "https://raw.githubusercontent.com/bonsalakot00/Test-Server/master/Data_2012.csv"
+url = "https://raw.githubusercontent.com/bonsalakot00/Test-Server/master/Data_2012.csv" #URL temporary storage of data
 webpage = urllib.request.urlopen(url)
-datareader = csv.reader(io.TextIOWrapper(webpage))
+datareader = csv.reader(io.TextIOWrapper(webpage))  #TextIOWrapper; buffered text interface to a buffered raw stream
 
-for row in datareader:
-    print(row)
+for row in datareader:  #loop object memory to display data
+    print(row)  #print object memory
 
 url = "https://raw.githubusercontent.com/bonsalakot00/Test-Server/master/Data_2013.csv"
 webpage = urllib.request.urlopen(url)
 datareader = csv.reader(io.TextIOWrapper(webpage))
+
+next(datareader)    #skip first line of data (which contains field names)
 
 for row in datareader:
     print(row)
@@ -20,12 +22,16 @@ url = "https://raw.githubusercontent.com/bonsalakot00/Test-Server/master/Data_20
 webpage = urllib.request.urlopen(url)
 datareader = csv.reader(io.TextIOWrapper(webpage))
 
+next(datareader)
+
 for row in datareader:
     print(row)
 
 url = "https://raw.githubusercontent.com/bonsalakot00/Test-Server/master/Data_2015.csv"
 webpage = urllib.request.urlopen(url)
 datareader = csv.reader(io.TextIOWrapper(webpage))
+
+next(datareader)
 
 for row in datareader:
     print(row)
@@ -34,12 +40,16 @@ url = "https://raw.githubusercontent.com/bonsalakot00/Test-Server/master/Data_20
 webpage = urllib.request.urlopen(url)
 datareader = csv.reader(io.TextIOWrapper(webpage))
 
+next(datareader)
+
 for row in datareader:
     print(row)
 
 url = "https://raw.githubusercontent.com/bonsalakot00/Test-Server/master/Data_2017.csv"
 webpage = urllib.request.urlopen(url)
 datareader = csv.reader(io.TextIOWrapper(webpage))
+
+next(datareader)
 
 for row in datareader:
     print(row)
